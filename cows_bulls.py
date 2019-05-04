f1279fdc0e6fd1efe85856d6b5675c71b62849b3
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-05-04 16:04:15 +0530

    Add a parameter to generate_random, ensure first digit not 0.

--- cows_bulls.py
+++ cows_bulls.py
@@ -24,13 +24,16 @@ def string_to_number(x):
     return guess
 
 
-def generate_random():
-    number = []
-    while len(number) < 4:
-        digit = random.randint(0, 9)
-        if number.count(digit) == 0:
-            number.append(digit)
-    return number
+def generate_random(n=4):
+    digits = []
+    while len(digits) < n:
+        if len(digits) == 0:
+            digit = random.randint(1, 9)
+        else:
+            digit = random.randint(0, 9)
+        if digits.count(digit) == 0:
+            digits.append(digit)
+    return digits
 
 
 if __name__ == "__main__":
